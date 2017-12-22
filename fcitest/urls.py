12a6f00400c732c225a01349fad2013c1d80a92f
Wang <chenchun.wang@nokia-sbell.com>
2017-12-22 11:20:03 +0800

    debug 1

---
+++ fcitest/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from . import view
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'fcitest.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', view.index),
+]
